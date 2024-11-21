a5a58eac6048e584a7a4e3368baeaddbeec85937
raghunandhanvr <raghunandhanvr@outlook.com>
2024-11-21 23:50:28 +0530

    new blog.

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
       <div className="sm:block hidden">
         <a href={socialLinks.github} target="_blank">
           <Image
-            src="/profile.jpeg"
+            src="/profile.jpg"
             alt="Profile photo"
             className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
             unoptimized
