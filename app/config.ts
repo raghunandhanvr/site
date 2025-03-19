ff4434212f87bc1ee064970a708813cf0ce679a5
raghunandhanvr <raghunandhanvr@outlook.com>
2025-03-19 12:09:41 +0530

    chore: update site URL to include 'www' for seo quick lookups without redirections

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  url: "https://raghu.app/",
+  url: "https://www.raghu.app/",
   name: "Raghunandhan VR",
   shortName: "Raghu",
   description: "I own a computer and I like to develop things with it.",
